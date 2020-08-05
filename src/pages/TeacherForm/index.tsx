15f8251f939b45d27c63749ff977a3e3c0961a87
Heymer Carmona de Britto <heymer_A7X@live.com>
2020-08-05 18:53:07 -0300

    Form de Cadastro de Aulas

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+
+import warningIcon from "../../assets/images/icons/warning.svg";
 
 import "./styles.css";
 
 function TeacherForm() {
   return (
-    <div id="page-teacher-list" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas."></PageHeader>
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo para começar a ensinar."
+      ></PageHeader>
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+          <Input name="name" label="Nome Completo"></Input>
+          <Input name="avatar" label="Avatar"></Input>
+          <Input name="whatsapp" label="WhatsApp"></Input>
+        </fieldset>
+        <fieldset>
+          <legend>Sobre a Aula</legend>
+          <Input name="subject" label="Matéria"></Input>
+          <Input
+            name="cost"
+            label="Custo da sua hora por aula"
+            type="currency"
+          ></Input>
+        </fieldset>
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar Cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,24 +3,16 @@ import PageHeader from "../../components/PageHeader";
 
 import "./styles.css";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os Proffys disponíveis.">
         <form action="" id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"></input>
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"></input>
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"></input>
-          </div>
+          <Input name="subject" label="Matéria"></Input>
+          <Input name="week_day" label="Dia da semana"></Input>
+          <Input name="time" label="Hora" type="time"></Input>
         </form>
       </PageHeader>
       <main>
