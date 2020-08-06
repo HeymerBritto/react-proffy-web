88080c4bb269728aaa285228c98d73c570df56d5
Heymer Carmona de Britto <heymer_A7X@live.com>
2020-08-06 08:35:49 -0300

    Connectando com o back-end

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,17 +1,46 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
 
-import "./styles.css";
 import TeacherItem from "../../components/TeacherItem";
 import Input from "../../components/Input";
+import Select from "../../components/Select";
+
+import "./styles.css";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os Proffys disponíveis.">
         <form action="" id="search-teachers">
-          <Input name="subject" label="Matéria"></Input>
-          <Input name="week_day" label="Dia da semana"></Input>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Portguês", label: "Portguês" },
+              { value: "T. I.", label: "T. I." },
+              { value: "Química", label: "Química" },
+            ]}
+          ></Select>
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-Feira" },
+              { value: "2", label: "Terça-Feira" },
+              { value: "3", label: "Quarta-Feira" },
+              { value: "4", label: "Quinta-Feira" },
+              { value: "5", label: "Sexta-Feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          ></Select>
           <Input name="time" label="Hora" type="time"></Input>
         </form>
       </PageHeader>
